0663aa51ecbfac4ce712106a43c492aa06f0fb7e
shubham jakhar <shubhamjakhar16@gmail.com>
2026-06-02 17:50:44 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,9 @@ app.get("/", (req, res) => {
   });
 });
 app.use("/user", userRouter);
+userRouter.get("/test", (req, res) => {
+    res.json({ message: "user route working" });
+});
 app.use((req, res, next) => {
   res.status(404).send("404 Page Not Found");
 });
